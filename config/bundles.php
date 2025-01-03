ef55dc818fa05436890b1173398b59f36900098e
volubyl01 <myriam.braconier@gmail.com>
2025-01-03 14:34:06 +0100

    Version de production

--- config/bundles.php
+++ config/bundles.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
